c68453792661e8b2a5fd352d2d8043f55b0e87fb
Ivaylo Iliev <ivaylo.il@gmail.com>
2015-05-26 10:43:13 +0300

    creating tweets actions rdy

--- ASP NET MVC/Twitter Web application/Twitter.Web/Controllers/TweetsController.cs
+++ ASP NET MVC/Twitter Web application/Twitter.Web/Controllers/TweetsController.cs
@@ -1,10 +1,17 @@
 ﻿namespace Twitter.Web.Controllers
 {
+    using System;
     using System.Web.Mvc;
+
+    using Microsoft.AspNet.Identity;
+
+    using Twitter.Data;
     using Twitter.Models;
 
     public class TweetsController : Controller
     {
+        TwitterDbContext db = new TwitterDbContext();
+
         [HttpGet]
         public ActionResult Create()
         {
@@ -14,14 +21,27 @@
         [HttpPost]
         public ActionResult Create(TweetInputModels tweet)
         {
-           
-            return null;
+            var newTweet = new TweetModel();
+            newTweet.Title = tweet.Title;
+            newTweet.Text = tweet.Text;
+            newTweet.URL = tweet.URL;
+            newTweet.Date = DateTime.Now;
+            newTweet.UserId = this.HttpContext.User.Identity.GetUserId();
+
+            this.db.Tweets.Add(newTweet);
+            this.db.SaveChanges();
             
+            return this.RedirectToAction("SuccessCreated");
+
         }
 
 //        public ActionResult Index()
 //        {
 //            return this.RedirectToRoute("~home/index");
 //        }
+        public ActionResult SuccessCreated()
+        {
+            return this.View();
+        }
     }
 }
